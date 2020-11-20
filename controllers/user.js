361f2e9b1ea8e1e29ec42834d1f4720cc3f9937e
lucas-science <lucaslhomme01@gmail.com>
2020-11-20 18:03:23 +0100

    ajout cookie lors de la création de compte

--- controllers/user.js
+++ controllers/user.js
@@ -2,40 +2,28 @@ const User = require('../models/user');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 
+
 exports.signup = (req, res, next) => {
-    bcrypt.hash(req.body.mdp, 15)
-        .then(hash => {
-            const user = new User({
-                pseudo: req.body.pseudo,
-                email: req.body.email,
-                mdp: hash
-            });
-            user.save()
-                .then(() => res.status(201).json({ message: "utilisateur créer" }))
-                .catch(error => res.status(500).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
+    bcrypt.hash(req.body.mdp, 15).then(function(hash) {
+        const user = new User({
+            pseudo: req.body.pseudo,
+            email: req.body.email,
+            mdp: hash
+        });
+        user.save()
+            .then(() => {
+                const token = jwt.sign({ userId: user._id }, secret, {
+                    expiresIn: '1h'
+                });
+                return res.cookie('token', token, { httpOnly: true }).status(200).json({ message: 'utilisateur créer' });
+            })
+            .catch(error => res.status(500).json({ error }));
+    });
 }
 
 
 const secret = "clef-secret"
 
-/*exports.signin = (req, res, next) => {
-    User.findOne({ email: req.body.email })
-        .then(user => {
-            if (!user) {
-                return res.status(401).json({ error: "utilisateur pas trouvé" })
-            }
-            bcrypt.compare(req.body.mdp, user.mdp)
-                .then(valid => {
-                    if (!valid) {
-                        return res.status(401).json({ error: "mot de passe incorrecte" })
-                    }
-                })
-                .catch(error => res.status(500).json({ error }));
-        })
-        .catch(error => res.status(500).json({ error }));
-}*/
 
 exports.signin = (req, res, next) => {
     User.findOne({ email: req.body.email }, function(err, user) {
@@ -67,7 +55,7 @@ exports.signin = (req, res, next) => {
                     const token = jwt.sign({ userId: user._id }, secret, {
                         expiresIn: '1h'
                     });
-                    return res.cookie('token', token, { httpOnly: true }).status(200).json({ message: 'nice' });
+                    return res.cookie('token', token, { httpOnly: true }).status(200).json({ message: 'connexion réussi' });
                 }
             });
         }
